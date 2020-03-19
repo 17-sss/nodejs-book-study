089cb301a747eab8eeecdf17570903458eb4b323
17-sss <jhsonking@naver.com>
2020-03-19 17:32:26 +0900

    200319_1732

--- 10-Web_API_Server_Make/nodebird-api/routes/middlewares.js
+++ 10-Web_API_Server_Make/nodebird-api/routes/middlewares.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');    // [10.3]
+const RateLimit = require('express-rate-limit');    // [10.6]
 
 exports.isLoggedIn = (req, res, next) => {
     if (req.isAuthenticated()) {
@@ -36,6 +37,30 @@ exports.verifyToken = (req, res, next) => {
         });
     }
 };
-
-
 // [10.3] END
+
+// [10.6] START
+exports.apiLimiter = new RateLimit({
+// windowMs: 기준시간
+    windowMs: 60 * 1000,    // 1분
+// max: 허용 횟수
+    max: 1,
+// delayMs: 호출 간격
+    delayMs: 0,
+// handler: 제한 초과시 콜백 함수
+    handler(req, res) {
+        res.status(this.statusCode).json({
+            code: this.statusCode, // 기본값 429
+            message: '1분에 한 번만 요청할 수 있습니다.',
+        });
+    },
+});
+
+// deprecated 미들웨어(메소드)는 사용하면 안되는 라우터에 붙일 것임.
+exports.deprecated = (req, res) => {
+    res.status(410).json({
+        code: 410,
+        message: '새로운 버전이 나왔습니다. 새로운 버전을 사용하세요.',
+    });
+};
+// [10.6] END
