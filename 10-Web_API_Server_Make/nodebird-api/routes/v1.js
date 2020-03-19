53fcd4c7807087a0eda50a390a2d7370da80a4e0
17-sss <jhsonking@naver.com>
2020-03-19 17:17:43 +0900

    200319_1717

--- 10-Web_API_Server_Make/nodebird-api/routes/v1.js
+++ 10-Web_API_Server_Make/nodebird-api/routes/v1.js
@@ -1,4 +1,4 @@
-// [10.3]
+// [10.3] START
 const express = require('express');
 const jwt = require('jsonwebtoken');
 
@@ -53,5 +53,56 @@ router.post('/token', async (req, res) => {
 router.get('/test', verifyToken, (req, res) => {
     res.json(req.decoded);
 });
+// [10.3] END
+
+
+// [10.5] START
+/* 
+    아래 2개의 라우터처럼 사용자에게 제공해도 되는 정보를 API로 만들면 됨.
+*/
+
+// 내가 올린 포스트를 가져오는 라우터
+router.get('/posts/my', verifyToken, (req, res) => {
+    Post.findAll({where: { userId: req.decoded.id }})
+        .then((posts) => {
+            console.log(posts);
+            res.json({
+                code: 200,
+                payload: posts,
+            });
+        })
+        .catch( (error) => {
+            console.error(error);
+            return res.status(500).json({
+                code: 500,
+                message: '서버 에러',
+            });
+        });
+});
+
+// 해시태그 검색 결과를 가져오는 라우터
+router.get('/posts/hashtag/:title', verifyToken, async(req, res) => {
+    try {
+        const hashtag = await Hashtag.find({where: {title: req.params.title }});
+        if(!hashtag) {
+            return res.status(404).json({
+                code: 404,
+                message: '검색 결과가 없습니다',
+            });
+        }
+        const posts = await hashtag.getPosts();
+        return res.json({
+            code: 200,
+            payload: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            code: 500,
+            message: '서버 에러',
+        });    
+    }
+});
+// [10.5] END
 
 module.exports = router;
