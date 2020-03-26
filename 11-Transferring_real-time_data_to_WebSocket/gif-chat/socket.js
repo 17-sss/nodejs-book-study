3650dd3c5c6b058440d44402177096e0c6758d28
17-sss <jhsonking@naver.com>
2020-03-26 11:41:27 +0900

    200326_1141

--- 11-Transferring_real-time_data_to_WebSocket/gif-chat/socket.js
+++ 11-Transferring_real-time_data_to_WebSocket/gif-chat/socket.js
@@ -1,26 +1,41 @@
-const WebSocket = require('ws');
+// Socket IO Version
+const SocketIO = require('socket.io');
 
 module.exports = (server) => {
-    const wss = new WebSocket.Server({server});
+    // 두번째 인자는 옵션
+    // 여기서는 클라이언트와 연결할 수 있는 경로를 의미하는 path만 사용
+    const io = SocketIO(server, {path: '/socket.io'});  
 
-    wss.on('connection', (ws, req) => {
+    // - connection: 클라이언트가 접속했을 때 발생하고, 콜백으로 소켓 객체(socket) 제공
+    io.on('connection', (socket) => {   // io와 socket 객체가 Socket.IO의 핵심
+        const req = socket.request;
+        /* 
+            - socket.request 속성으로 요청 객체에 접근 가능
+            - socket.request.res는 응답 객체에 접근 가능
+        */
         const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-        console.log('새로운 클라이언트 접속', ip);
-        ws.on('message', (message) => {
-            console.log(message);
+        // socket.id로 소켓 고유 아이디 가져올 수 있음
+        console.log('새로운 클라이언트 접속', ip, socket.id, req.ip);   
+
+        socket.on('disconnect', () => { // disconnect, 클라이언트가 연결을 끊었을 때 발생
+            console.log('클라이언트 접속 해제', ip, socket.id);
+            clearInterval(socket.interval);
         });
-        ws.on('error', (error) => {
+        socket.on('error', (error) => { // error, 통신 과정 중 에러가 나왔을 때 발생
             console.error(error);
         });
-        ws.on('close', () => {
-            console.log('클라이언트 접속 해제', ip);
-            clearInterval(ws.interval);
+        socket.on('reply', (data) => { // reply는 사용자가 직접 만든 이벤트
+            // 클라이언트에서 reply라는 이벤트명으로 데이터 보낼 때 서버에서 받는 부분.
+            console.log(data);
         });
-        const interval = setInterval(()=> {
-            if(ws.readyState === ws.OPEN) {
-                ws.send('서버에서 클라이언트로 메서지를 보냅니다.');
-            }
+        socket.interval = setInterval(() => {
+            /* 
+                첫 번째 인자는 이벤트 이름, 두 번째 인자는 데이터
+                news라는 이벤트이름으로 Hello Socket.IO 라는 데이터를 클라이언트에 보낸것.
+                클라이언트가 이 메세지를 받기 위해서는 news 이벤트 리스너를 만들어두어야함 (뷰 부분에)
+            */
+            socket.emit('news', 'Hello Socket.IO'); 
+
         }, 3000);
-        ws.interval = interval;
     });
 };
