a8d01eb4de103fbe08ef6cddb858f02ad8ecb495
17-sss <jhsonking@naver.com>
2020-03-26 22:04:19 +0900

    200326_2204

---
+++ 11-Transferring_real-time_data_to_WebSocket/gif-chat/schemas/chat.js
@@ -0,0 +1,30 @@
+// [11.4] 채팅 스키마
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const { Types: {ObjectId} } = Schema;
+const chatSchema = new Schema({
+    //- room 필드는 Room 스키마와 연결하여 Room 컬렉션의 ObjectId가 들어가게 됨.
+    room: {         // 채팅방 아이디
+        type: ObjectId,
+        required: true,
+        ref: 'Room',
+    },
+    user: {         // 채팅을 한 사람
+        type: String,
+        required: true,
+    }, 
+    /* 
+        - chat 또는 img 필드에 require 속성이 없는 이유는 
+            채팅 메세지나 GIF이미지 둘 중 하나만 저장하면 되기 때문
+    */
+    chat: String,   // 채팅 내역
+    gif: String,    // GIF 이미지 주소
+    createdAt: {    // 채팅 시간
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Room', roomSchema);
+
---
+++ 11-Transferring_real-time_data_to_WebSocket/gif-chat/schemas/room.js
@@ -0,0 +1,27 @@
+// [11.4] 채팅방 스키마
+const mongoose = require('mongoose');
+
+const { Schema } = mongoose;
+const roomSchema = new Schema({
+    title: {            // 방 제목
+        type: String,
+        required: true,
+    },
+    max: {              // 최대 수용 인원
+        type: Number,
+        required: true,
+        defaultValue: 10,   //- 수용인원은 기본적으로 10명
+        min: 2,             //- 최소 2명 이상으로 설정
+    },
+    owner: {            // 방장
+        type: String,
+        required: true,
+    },
+    password: String,   // 비밀번호(비번 설정하면 비밀방, 아니라면 공개방)
+    createdAt: {        // 생성 시간
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Room', roomSchema);
