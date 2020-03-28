40812bf3af9537b8f41a29f01b9a95986d5fa68f
17-sss <jhsonking@naver.com>
2020-03-28 17:42:17 +0900

    200328_1742

--- 11-Transferring_real-time_data_to_WebSocket/gif-chat/routes/index.js
+++ 11-Transferring_real-time_data_to_WebSocket/gif-chat/routes/index.js
@@ -1,9 +1,104 @@
+// [11.4 : 17.] (생성)
 const express = require('express');
+
+const Room = require('../schemas/room');
+const Chat = require('../schemas/chat');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index');
+//- 채팅방 목록이 보이는 메인 화면을 렌더링하는 라우터 (get('/'))
+router.get('/', async (req, res, next) => {
+    try {
+        const rooms = await Room.find({});
+        res.render('main', { rooms, title: 'GIF 채팅방', error: req.flash('roomError')});
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+//- 채팅방 생성화면을 렌더링하는 라우터 (get('/room'))
+router.get('/room', (req, res) => {
+    res.render('room', { title: 'GIF 채팅방 생성'});
+});
+
+//- 채팅방을 만드는 라우터 (post('/room'))
+router.post('/room', async (req, res, next) => { // 1
+    try {
+        const room = new Room({
+            title: req.body.title,
+            max: req.body.max,
+            owner: req.session.color,
+            password: req.body.password,
+        });
+
+        const newRoom = await room.save();
+
+        // app.set('io', io)로 저장했던 io객체를 req.app.get('io')로 가져옴
+        const io = req.app.get('io');
+        
+        // io.of('/room').emit 메서드는 /room 네임스페이스에 연결한 모든 클라이언트에게 데이터를 보내는 메서드
+        //+ 네임스페이스가 따로 없는 경우, io.emit 메서드로 모든 클라이언트에게 데이터를 보낼 수 있음.
+        io.of('/room').emit('newRoom', newRoom);
+
+        res.redirect(`/room/${newRoom._id}?password=${req.body.password}`);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+});
+
+//- 채팅방을 렌더링하는 라우터 (get('/room/:id))
+/* 
+    + 렌더링 전에 방이 존재하는지, 비밀방인 경우 비번이 맞는지, 허용인원 초과하지 않았는지 검사.
+        io.of('/chat').adapter.rooms에 방목록이 들어있음.
+        io.of('/chat').adapter.rooms[req.parms.id]를 하면 해당 방의 소켓 목록이 나옴.
+            => 이것으로 소켓의 수를 세서 참가인원 수를 알아낼수 있음
+*/
+router.get('/room/:id', async (req, res, next) => { // 2
+    try {
+        const room = await Room.findOne({_id: req.params.id});
+        const io = req.app.get('io');
+        if (!room) {
+            req.flash('roomError', '존재하지 않는 방입니다.');
+            return res.redirect('/');
+        }
+        if (room.password && room.password !== req.query.password) {
+            req.flash('roomError', '비밀번호가 틀렸습니다.');
+            return res.redirect('/');
+        }
+
+        const { rooms } = io.of('/chat').adapter;
+        if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
+            req.flash('roomError', '허용인원이 초과하였습니다.');
+            return res.redirect('/');
+        }
+        return res.render('chat', {
+            room,
+            title: room.title,
+            chats: [],
+            user: req.session.color,
+        });
+    } catch (error) {
+        console.error(error);
+        return next(error);
+    }
+});
+
+//- 채팅방을 삭제하는 라우터 (delete('/room/:id'))
+router.delete('/room/:id', async(req, res, next) => {   // 3
+    try {
+        await Room.remove({_id: req.params.id});
+        await Chat.remove({room: req.params.id});
+        res.send('ok');
+
+        setTimeout(() => {
+           req.app.get('io').of('/room').emit('removeRoom', req.params.id);
+        }, 2000);
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
 });
 
 module.exports = router;
--- 11-Transferring_real-time_data_to_WebSocket/gif-chat/socket.js
+++ 11-Transferring_real-time_data_to_WebSocket/gif-chat/socket.js
@@ -1,7 +1,8 @@
 // [11.4] (/room, /chat 웹 소켓 이벤트 연결)
 const SocketIO = require('socket.io');
+const axios = require('axios');     // [11.4 : 16.] (socket에 express-session 적용)
 
-module.exports = (server, app) => {
+module.exports = (server, app, sessionMiddleware) => { // [11.4 : 16.] sessionMiddleware 추가
     const io = SocketIO(server, {path: '/socket.io'});  
 
     app.set('io', io);  //+ req.app.get('io')로 접근 가능
@@ -14,6 +15,16 @@ module.exports = (server, app) => {
     */
     const room = io.of('/room');
     const chat = io.of('/chat');
+
+    // [11.4 : 16.] (socket에 express-session 적용) START --
+    io.use((socket, next) => { 
+        /* 
+            io.use 메서드에 미들웨어 장착
+            세션 미들웨어에 요청 객체, 응답 객체, next 함수를 넣어주면 됨.
+        */
+        sessionMiddleware(socket.request, socket.request.res, next);
+    });
+    // [11.4 : 16.] (socket에 express-session 적용) END --
 
     // /room 네임스페이스에 이벤트 리스너 붙임
     room.on('connection', (socket) => { 
@@ -36,9 +47,37 @@ module.exports = (server, app) => {
         
             socket.join(roomId);    // 채팅방에 들어가는 메서드 (접속 시)
 
+            // [11.4 : 16.] (socket에 express-session 적용) START --
+            //- socket.to(방 아이디): 특정방에 데이터를 보냄
+            socket.to(roomId).emit('join', {
+                user: 'system',
+                chat: `${req.session.color}님이 입장하셨습니다.`,
+            });
+            // [11.4 : 16.] (socket에 express-session 적용) END --
+
         socket.on('disconnect', () => {
             console.log('chat 네임스페이스 접속 해제');
             socket.leave(roomId);   // 채팅방에서 나가는 메서드 (접속 해제 시)
+            
+            // [11.4 : 16.] (socket에 express-session 적용) START --
+            //- 접속 해제 시, 현재 방의 사람 수를 구해서 참여자 수가 0명이면 방을 제거하는 HTTP 요청을 보냄
+            const currentRoom = socket.adapter.rooms[roomId];   // 참여중인 소켓정보가 들어있음
+            const userCount = currentRoom ? currentRoom.length : 0;
+            if (userCount === 0) {  // 0명이면 방 삭제
+                axios.delete(`http://localhost:8005/room/${roomId}`)
+                    .then(() => {
+                        console.log('방 제거 요청 성공');
+                    })
+                    .catch((error) => {
+                        console.error(error);
+                    });
+            } else {    // 남은 참여자가 0명이 아니면, 누가 나갔는지 퇴장했다는 데이터를 보냄
+                socket.to(roomId).emit('exit', {
+                    user: 'system',
+                    chat: `${req.session.color}님이 퇴장하셨습니다.`,
+                });
+            }
+            // [11.4 : 16.] (socket에 express-session 적용) END --
         });
     });
 };
