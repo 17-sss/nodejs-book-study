bb58aeb2fd61224fd5223cd57c57e1895e683eec
17-sss <jhsonking@naver.com>
2020-04-09 17:17:22 +0900

    200409_1717

--- 13-Google_API_Place_Search/node-place/routes/_bak/[13,2]_index.js
+++ 13-Google_API_Place_Search/node-place/routes/_bak/[13,2]_index.js
@@ -4,7 +4,6 @@ const util = require('util');
 const googleMaps = require('@google/maps'); 
 
 const History = require('../schemas/history');
-const Favorite = require('../schemas/favorite');
 
 const router = express.Router();
 
@@ -50,11 +49,7 @@ router.get('/search/:query', async(req, res, next) => {
             바꿀 수 있는 콜백은 프로미스로 바꿔서 async/await 문법 사용하는 것이 깔끔
     */
     const googlePlaces = util.promisify(googleMapsClient.places); // 구글 지도 클라이언트의 places 메서드: 장소 검색
-
-    // [13.4 : 02.] START ---
-    const googlePlacesNearby = util.promisify(googleMapsClient.placesNearby);
-    const { lat, lng, type } = req.query;
-
+    
     try {
         const history = new History({
             query: req.params.query,
@@ -62,72 +57,20 @@ router.get('/search/:query', async(req, res, next) => {
         await history.save();
         // ▲ 결괏값 반환 전, 검색 내역을 구현하기 위해 데이터베이스에 검색어 저장
 
-        let response;
-
-        if (lat && lng) {            
-            // 만약 쿼리스트링으로 lat과 lng가 제공되면 places API 대신 placesNearby API 사용            
-            response = await googlePlacesNearby({
-                keyword: req.params.query,  // 찾을 검색어
-                location: `${lat}, ${lng}`, // 위도와 경도
-                rankby: 'distance',         // 정렬 순서 (가까운 거리순 정렬)
-                // radius: 5000,   // 인기순으로 정렬 (검색반경 5000) | rankby와 radius 중 둘중하나만 써야하는 듯.
-                language: 'ko',             // 검색 언어
-                type,
-            });
-        } else {
-            response = await googlePlaces({
-                query: req.params.query,    // 검색어
-                language: 'ko',             // 한국어로 설정
-                type,
-            });
-        }        
-        // [13.4 : 02.]  END ---
+        const response = await googlePlaces({
+            query: req.params.query,    // 검색어
+            language: 'ko',             // 한국어로 설정
+        });
 
         res.render('result', {
             title: `${req.params.query} 검색 결과`,
             results: response.json.results, // 결괏값 들어 있음.
             query: req.params.query,
         });
-        
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }    
-});
-
-
-// [13.4 : 06.] START
-router.post('/location/:id/favorite', async (req, res, next) => {
-    try {
-        const favorite = await Favorite.create({
-            placeId: req.params.id,
-            name: req.body.name,
-
-            // 주의! 
-                // 보통은 Google Maps API 사용시엔 위도(lat) 경도(lng)순이지만
-                // 여기선 (Favorite 스키마_몽구스) 역순으로 넣어야함. 경도, 위도 순으로
-            location: [req.body.lng, req.body.lat],
-        });
-
-        res.send(favorite);
     } catch (error) {
         console.error(error);
         next(error);
     }
 });
-// [13.4 : 06.] END
-
-// [13.5 : 02.] START
-router.get('/', async(req, res, next) => {
-    try {
-        const favorites = await Favorite.find({});
-        res.render('index', {results: favorites });
-    } catch (error) {
-        console.error(error);
-        next(error);
-    }
-});
-// [13.5 : 02.] END
-
 
 module.exports = router;
